c913f6b5664aef48c410c8c64fd474db535288c1
Ramon Saavedra <saavedra.ramon.brand@gmail.com>
2024-06-05 10:33:11 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import  { ThemeProvider } from "styled-components";
-import Testing from "./components/Testing";
-import theme from "./theme/ThemeColors";
-
-
+import { ThemeProvider } from 'styled-components';
+import Testing from './components/Testing';
+import theme from './theme/ThemeColors';
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <div>App</div>
-      <Testing/>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <div>App</div>
+            <Testing />
+        </ThemeProvider>
+    );
 };
 
 export default App;
